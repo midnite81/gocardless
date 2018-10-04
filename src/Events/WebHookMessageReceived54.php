5ffda422a516483928ac0e1c99128a7fa12e36f8
Simon Rogers <webdev@centralblue.co.uk>
2018-10-04 11:13:15 +0100

    Update use statements

--- src/Events/WebHookMessageReceived54.php
+++ src/Events/WebHookMessageReceived54.php
@@ -2,6 +2,9 @@
 
 namespace Midnite81\GoCardless\Events;
 
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class WebHookMessageReceived54 extends AbstractWebHook
 {
     use Dispatchable, SerializesModels;

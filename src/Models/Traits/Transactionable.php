2a5b359e547ac30f0d366ef6ce185e541ab996f4
Simon Rogers <webdev@centralblue.co.uk>
2018-10-11 15:32:24 +0100

    Add transcation method

--- src/Models/Traits/Transactionable.php
+++ src/Models/Traits/Transactionable.php
@@ -12,4 +12,26 @@ trait Transactionable
     {
         return $this->morphMany(Transaction::class, 'transactionable');
     }
+
+    /**
+     * Create a transaction from the parent model
+     *
+     * @param       $action
+     * @param array $data
+     */
+    public function addTransaction($action, array $data)
+    {
+        $transaction = $this->transactions()->create([
+           'action' => $action
+        ]);
+
+        if (! empty($data)) {
+            foreach($data as $key=>$value) {
+                $transaction->data()->create([
+                    'key' => $key,
+                    'value' => (! is_string($value)) ? json_encode($value) : $value,
+                ]);
+            }
+        }
+    }
 }
